fcb176b4dc1f5af33cb61a26a5260a5ef5f7a060
ElisabettaMirti <belmirti@gmail.com>
2024-05-20 12:03:50 +0200

    inseriti altri passaggi nello pseudocodice di base e iniziato con il dichiarare la variabile del bottone

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 
 const gridEL = document.querySelector('section.grid');
 
+//>Creo una costante per il bottone del play
+
+const playButton = document.querySelector('button');
+
 // >Creo un ciclo per creare gli articoli-quadrati all'interno della griglia
 //     >All'interno del ciclo, ogni elemento creato va poi appeso al contenitore
 // >Aggiungo un ascoltatore che aggiunga una classe che dà background azzurro ogni volta che una cella viene cliccata e
 //     un console.log che stampi il numero indice corrispondente
+
+
+//>inserisco un eventListener per l'attivazione del bottone
+    //>Prima di ricaricare la griglia, deve essere svuotato l'innerHTML
+
+
 
 for (let i = 0; i < 100; i++){
     const squareEl = document.createElement('article');
